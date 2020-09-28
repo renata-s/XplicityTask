c96ae443eed42862423aa11153eae26ef8d63d50
Renata Simaitiene <simaitiene.renata@gmail.com>
2020-09-28 20:10:52 +0300

    add line button

--- src/views/ProductLine.vue
+++ src/views/ProductLine.vue
@@ -1,10 +1,10 @@
 <template>
   <tr>
-    <td><v-text-field v-model="line.code"></v-text-field></td>
-    <td><v-text-field v-model="line.name"></v-text-field></td>
-    <td><v-text-field v-model="line.price"></v-text-field></td>
-    <td><v-text-field v-model="line.tax"></v-text-field></td>
-    <td><v-text-field v-model="TotalPrice"></v-text-field></td>
+    <td><v-text-field flat solo v-model="line.code"></v-text-field></td>
+    <td><v-text-field flat solo v-model="line.name"></v-text-field></td>
+    <td><v-text-field flat solo v-model="line.price"></v-text-field></td>
+    <td><v-text-field flat solo v-model="line.tax"></v-text-field></td>
+    <td><v-text-field flat solo v-model="TotalPrice"></v-text-field></td>
   </tr>
 </template>
 <script>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,32 +1,35 @@
 <template>
-  <v-simple-table>
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-left">
-            Code
-          </th>
-          <th class="text-left">
-            Name
-          </th>
-          <th class="text-left">
-            Price (EUR)
-          </th>
-          <th class="text-left">
-            TAX
-          </th>
-          <th class="text-left">
-            Price + TAX (EUR)
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <template v-for="(item, index) in prices">
-          <ProductLine :key="index" :line="item" />
-        </template>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <div>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">
+              Code
+            </th>
+            <th class="text-left">
+              Name
+            </th>
+            <th class="text-left">
+              Price (EUR)
+            </th>
+            <th class="text-left">
+              TAX
+            </th>
+            <th class="text-left">
+              Price + TAX (EUR)
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <template v-for="(item, index) in prices">
+            <ProductLine :key="index" :line="item" />
+          </template>
+        </tbody>
+      </template>
+    </v-simple-table>
+    <v-btn elevation="2" @click="AddLine">Add line</v-btn>
+  </div>
 </template>
 
 <script>
@@ -43,6 +46,17 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    AddLine() {
+      this.prices.push({
+        id: 1,
+        code: "",
+        name: "",
+        price: "",
+        tax: 21
+      });
+    }
   }
 };
 </script>
